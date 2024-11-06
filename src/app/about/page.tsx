d830c510af6f87352852f515dd12e49f0c942f68
Paul Sumner <paulsum1985@hotmail.com>
2024-11-06 08:41:15 +0000

    adsense verify

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -52,7 +52,7 @@ const AboutContact = () => {
                   We value your feedback and questions. You can reach us through any of the following methods:
                 </p>
                 <ul className="list-disc ml-6 mt-2">
-                  <li>Email: <a href="mailto:paulsum1985@hotmail.com" className="text-violet-600 hover:text-violet-700">your-email@example.com</a></li>
+                  <li>Email: <a href="mailto:paulsum1985@hotmail.com" className="text-violet-600 hover:text-violet-700">paulsum1985@hotmail.com</a></li>
                   <li>GitHub: <a href="https://github.com/Paulsum1985" target="_blank" rel="noopener noreferrer" className="text-violet-600 hover:text-violet-700">Create an issue in our repository</a></li>
                 </ul>
               </section>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <head>
         <Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbyard.js"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1522061448979634"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
