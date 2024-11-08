2da9d7346152a1d66e580a5528808fb2f829cbf6
Paul Sumner <paulsum1985@hotmail.com>
2024-11-08 14:49:23 +0000

    favicon sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -287,6 +287,9 @@ return (
   <meta property="twitter:image" content="https://protein-calculator.co.uk/protein-calculator-og.jpg" />
 
   <link rel="icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 </Head>
     <div className="max-w-4xl mx-auto">
       <div className="mb-4 space-y-4">
