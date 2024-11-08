c912db592b205698c9148aac4bb3116b98a95e44
Paul Sumner <paulsum1985@hotmail.com>
2024-11-08 08:23:23 +0000

    SEO

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  
+  // Add this new section
+  async rewrites() {
+    return [
+      {
+        source: '/robots.txt',
+        destination: '/api/robots'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
